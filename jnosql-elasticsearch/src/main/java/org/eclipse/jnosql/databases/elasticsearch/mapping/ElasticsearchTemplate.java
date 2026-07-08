dd378efeab5be98ffd7751b320cdf44b6ed36dd3
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-08 18:23:55 +0100

    update ElasticsearchTemplate to expand native search contract documentation and usage example

--- jnosql-elasticsearch/src/main/java/org/eclipse/jnosql/databases/elasticsearch/mapping/ElasticsearchTemplate.java
+++ jnosql-elasticsearch/src/main/java/org/eclipse/jnosql/databases/elasticsearch/mapping/ElasticsearchTemplate.java
@@ -43,7 +43,7 @@ import java.util.stream.Stream;
  *
  * <pre>{@code
  * @Inject
- * ElasticsearchTemplate elasticsearchTemplate;
+ * private ElasticsearchTemplate elasticsearchTemplate;
  *
  * SearchRequest request = new SearchRequest.Builder()
  *         .index("documents")
