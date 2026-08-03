a150fcf5ff8f68938bba776cff763ddc368df6ef
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-03 08:39:33 +0100

    docs: add class javadoc

--- jnosql-dynamodb/src/main/java/org/eclipse/jnosql/databases/dynamodb/communication/DefaultDynamoDBDatabaseManager.java
+++ jnosql-dynamodb/src/main/java/org/eclipse/jnosql/databases/dynamodb/communication/DefaultDynamoDBDatabaseManager.java
@@ -62,6 +62,9 @@ import static org.eclipse.jnosql.databases.dynamodb.communication.DynamoDBConver
 import static org.eclipse.jnosql.databases.dynamodb.communication.DynamoDBConverter.toItem;
 import static org.eclipse.jnosql.databases.dynamodb.communication.DynamoDBConverter.toItemUpdate;
 
+/**
+ * Provides default dynamo dbdatabase manager support.
+ */
 public class DefaultDynamoDBDatabaseManager implements DynamoDBDatabaseManager {
 
     private final String database;
@@ -74,6 +77,13 @@ public class DefaultDynamoDBDatabaseManager implements DynamoDBDatabaseManager {
 
     private final ConcurrentHashMap<String, DescribeTableResponse> tables = new ConcurrentHashMap<>();
 
+/**
+ * Returns the default dynamo dbdatabase manager.
+ *
+ * @param database the database
+ * @param dynamoDbClient the dynamo db client
+ * @param settings the settings
+ */
     public DefaultDynamoDBDatabaseManager(String database, DynamoDbClient dynamoDbClient, Settings settings) {
         this.settings = settings;
         this.database = database;
