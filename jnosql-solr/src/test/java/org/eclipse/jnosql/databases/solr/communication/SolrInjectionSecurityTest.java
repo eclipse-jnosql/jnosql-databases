61501b861cd709e7fad8265749ab678cfee85d40
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-07 11:34:12 +0100

    fix(solr): route all client operations through configured database

---
+++ jnosql-solr/src/test/java/org/eclipse/jnosql/databases/solr/communication/SolrInjectionSecurityTest.java
@@ -0,0 +1,92 @@
+/*
+ *  Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+
+package org.eclipse.jnosql.databases.solr.communication;
+
+import org.apache.solr.client.solrj.util.ClientUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.jnosql.communication.semistructured.DeleteQuery.delete;
+import static org.eclipse.jnosql.communication.semistructured.SelectQuery.select;
+
+class SolrInjectionSecurityTest {
+
+    @Test
+    void shouldEscapeSelectQueryConditionValue() {
+        String payload = "[* TO *] OR _entity:Secret";
+        var query = select().from("Public").where("name").eq(payload).build();
+
+        String solr = DocumentQueryConverter.convert(query);
+
+        assertThat(solr).isEqualTo("_entity:Public AND name:" + ClientUtils.escapeQueryChars(payload));
+        assertThat(solr).doesNotContain("name:" + payload);
+    }
+
+    @Test
+    void shouldEscapeDeleteQueryConditionValue() {
+        String payload = "nope OR *:*";
+        var query = delete().from("Comment").where("_id").eq(payload).build();
+
+        String solr = DocumentQueryConverter.convert(query);
+
+        assertThat(solr).isEqualTo("_entity:Comment AND _id:" + ClientUtils.escapeQueryChars(payload));
+        assertThat(solr).doesNotContain("_id:" + payload);
+    }
+
+    @Test
+    void shouldEscapeInConditionValues() {
+        var query = select().from("Person")
+                .where("name").in(asList("Ada", "[* TO *]"))
+                .build();
+
+        String solr = DocumentQueryConverter.convert(query);
+
+        assertThat(solr).isEqualTo("_entity:Person AND name:(Ada OR "
+                + ClientUtils.escapeQueryChars("[* TO *]") + ')');
+    }
+
+    @Test
+    void shouldPreserveLikeWildcardsAndEscapeOperators() {
+        String payload = "Lu* OR _entity:Secret";
+        var query = select().from("Public").where("name").like(payload).build();
+
+        String solr = DocumentQueryConverter.convert(query);
+
+        assertThat(solr).isEqualTo("_entity:Public AND name:Lu*\\ OR\\ _entity\\:Secret");
+        assertThat(solr).doesNotContain(" OR _entity");
+    }
+
+    @Test
+    void shouldEscapeNativeQueryParams() {
+        String payload = "[* TO *] OR _entity:Secret";
+
+        String solr = DefaultSolrDocumentManager.bindNativeQuery("name:@name AND _entity:person",
+                Map.of("name", payload));
+
+        assertThat(solr).isEqualTo("name:" + ClientUtils.escapeQueryChars(payload) + " AND _entity:person");
+    }
+
+    @Test
+    void shouldReplaceLongerNativeQueryParamsFirst() {
+        String solr = DefaultSolrDocumentManager.bindNativeQuery("id:@id OR id2:@id2",
+                Map.of("id", "1", "id2", "2 OR *:*"));
+
+        assertThat(solr).isEqualTo("id:1 OR id2:" + ClientUtils.escapeQueryChars("2 OR *:*"));
+    }
+}
