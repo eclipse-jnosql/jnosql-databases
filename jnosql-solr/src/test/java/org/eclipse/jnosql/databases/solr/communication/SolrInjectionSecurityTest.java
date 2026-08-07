8be5b6425444b5832374c0eb46e7b6d5568c138d
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-07 11:42:18 +0100

    test(solr): improve injection security test structure and readability

--- jnosql-solr/src/test/java/org/eclipse/jnosql/databases/solr/communication/SolrInjectionSecurityTest.java
+++ jnosql-solr/src/test/java/org/eclipse/jnosql/databases/solr/communication/SolrInjectionSecurityTest.java
@@ -16,6 +16,8 @@
 package org.eclipse.jnosql.databases.solr.communication;
 
 import org.apache.solr.client.solrj.util.ClientUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -25,68 +27,85 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.jnosql.communication.semistructured.DeleteQuery.delete;
 import static org.eclipse.jnosql.communication.semistructured.SelectQuery.select;
 
+@DisplayName("Solr injection security")
 class SolrInjectionSecurityTest {
 
-    @Test
-    void shouldEscapeSelectQueryConditionValue() {
-        String payload = "[* TO *] OR _entity:Secret";
-        var query = select().from("Public").where("name").eq(payload).build();
+    @Nested
+    @DisplayName("When converting generated Solr queries")
+    class WhenConvertingGeneratedSolrQueries {
 
-        String solr = DocumentQueryConverter.convert(query);
+        @Test
+        @DisplayName("Should escape select condition values that contain Lucene operators")
+        void shouldEscapeSelectQueryConditionValue() {
+            String payload = "[* TO *] OR _entity:Secret";
+            var query = select().from("Public").where("name").eq(payload).build();
 
-        assertThat(solr).isEqualTo("_entity:Public AND name:" + ClientUtils.escapeQueryChars(payload));
-        assertThat(solr).doesNotContain("name:" + payload);
+            String solr = DocumentQueryConverter.convert(query);
+
+            assertThat(solr).isEqualTo("_entity:Public AND name:" + ClientUtils.escapeQueryChars(payload));
+            assertThat(solr).doesNotContain("name:" + payload);
+        }
+
+        @Test
+        @DisplayName("Should escape delete condition values that could broaden deleteByQuery")
+        void shouldEscapeDeleteQueryConditionValue() {
+            String payload = "nope OR *:*";
+            var query = delete().from("Comment").where("_id").eq(payload).build();
+
+            String solr = DocumentQueryConverter.convert(query);
+
+            assertThat(solr).isEqualTo("_entity:Comment AND _id:" + ClientUtils.escapeQueryChars(payload));
+            assertThat(solr).doesNotContain("_id:" + payload);
+        }
+
+        @Test
+        @DisplayName("Should escape every value in an IN condition")
+        void shouldEscapeInConditionValues() {
+            var query = select().from("Person")
+                    .where("name").in(asList("Ada", "[* TO *]"))
+                    .build();
+
+            String solr = DocumentQueryConverter.convert(query);
+
+            assertThat(solr).isEqualTo("_entity:Person AND name:(Ada OR "
+                    + ClientUtils.escapeQueryChars("[* TO *]") + ')');
+        }
+
+        @Test
+        @DisplayName("Should preserve LIKE wildcards while escaping injected operators")
+        void shouldPreserveLikeWildcardsAndEscapeOperators() {
+            String payload = "Lu* OR _entity:Secret";
+            var query = select().from("Public").where("name").like(payload).build();
+
+            String solr = DocumentQueryConverter.convert(query);
+
+            assertThat(solr).isEqualTo("_entity:Public AND name:Lu*\\ OR\\ _entity\\:Secret");
+            assertThat(solr).doesNotContain(" OR _entity");
+        }
     }
 
-    @Test
-    void shouldEscapeDeleteQueryConditionValue() {
-        String payload = "nope OR *:*";
-        var query = delete().from("Comment").where("_id").eq(payload).build();
+    @Nested
+    @DisplayName("When binding native Solr query parameters")
+    class WhenBindingNativeSolrQueryParameters {
 
-        String solr = DocumentQueryConverter.convert(query);
+        @Test
+        @DisplayName("Should escape native query parameter values")
+        void shouldEscapeNativeQueryParams() {
+            String payload = "[* TO *] OR _entity:Secret";
 
-        assertThat(solr).isEqualTo("_entity:Comment AND _id:" + ClientUtils.escapeQueryChars(payload));
-        assertThat(solr).doesNotContain("_id:" + payload);
-    }
+            String solr = DefaultSolrDocumentManager.bindNativeQuery("name:@name AND _entity:person",
+                    Map.of("name", payload));
 
-    @Test
-    void shouldEscapeInConditionValues() {
-        var query = select().from("Person")
-                .where("name").in(asList("Ada", "[* TO *]"))
-                .build();
+            assertThat(solr).isEqualTo("name:" + ClientUtils.escapeQueryChars(payload) + " AND _entity:person");
+        }
 
-        String solr = DocumentQueryConverter.convert(query);
+        @Test
+        @DisplayName("Should replace longer parameter names before shorter prefixes")
+        void shouldReplaceLongerNativeQueryParamsFirst() {
+            String solr = DefaultSolrDocumentManager.bindNativeQuery("id:@id OR id2:@id2",
+                    Map.of("id", "1", "id2", "2 OR *:*"));
 
-        assertThat(solr).isEqualTo("_entity:Person AND name:(Ada OR "
-                + ClientUtils.escapeQueryChars("[* TO *]") + ')');
-    }
-
-    @Test
-    void shouldPreserveLikeWildcardsAndEscapeOperators() {
-        String payload = "Lu* OR _entity:Secret";
-        var query = select().from("Public").where("name").like(payload).build();
-
-        String solr = DocumentQueryConverter.convert(query);
-
-        assertThat(solr).isEqualTo("_entity:Public AND name:Lu*\\ OR\\ _entity\\:Secret");
-        assertThat(solr).doesNotContain(" OR _entity");
-    }
-
-    @Test
-    void shouldEscapeNativeQueryParams() {
-        String payload = "[* TO *] OR _entity:Secret";
-
-        String solr = DefaultSolrDocumentManager.bindNativeQuery("name:@name AND _entity:person",
-                Map.of("name", payload));
-
-        assertThat(solr).isEqualTo("name:" + ClientUtils.escapeQueryChars(payload) + " AND _entity:person");
-    }
-
-    @Test
-    void shouldReplaceLongerNativeQueryParamsFirst() {
-        String solr = DefaultSolrDocumentManager.bindNativeQuery("id:@id OR id2:@id2",
-                Map.of("id", "1", "id2", "2 OR *:*"));
-
-        assertThat(solr).isEqualTo("id:1 OR id2:" + ClientUtils.escapeQueryChars("2 OR *:*"));
+            assertThat(solr).isEqualTo("id:1 OR id2:" + ClientUtils.escapeQueryChars("2 OR *:*"));
+        }
     }
 }
