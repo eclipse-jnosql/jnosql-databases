f6531fb9eb7218b8c1836e5cfe4cdb4471fa1122
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-11 13:29:15 +0100

    test(cassandra): cover query identifier injection

---
+++ jnosql-cassandra/src/test/java/org/eclipse/jnosql/databases/cassandra/communication/QueryUtilsSecurityTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * and Apache License v2.0 which accompanies this distribution.
+ */
+package org.eclipse.jnosql.databases.cassandra.communication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class QueryUtilsSecurityTest {
+
+    @Test
+    void shouldQuoteCqlIdentifierPayload() {
+        assertThat(QueryUtils.getName("name\" = 'x' WHERE token(id) > 0 --"))
+                .isEqualTo("\"name\"\" = 'x' WHERE token(id) > 0 --\"");
+    }
+
+    @Test
+    void shouldQuoteCountQueryIdentifiers() {
+        assertThat(QueryUtils.count("users; DROP TABLE secrets", "tenant"))
+                .isEqualTo("select count(*) from tenant.\"users; DROP TABLE secrets\"");
+    }
+}
