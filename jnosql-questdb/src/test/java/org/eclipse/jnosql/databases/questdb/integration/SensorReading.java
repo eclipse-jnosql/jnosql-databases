66aebeeac6e0fdee750236fcc5a13200ac49efcb
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-15 04:31:39 +0100

    test(questdb): add SensorReading record for integration test purposes

---
+++ jnosql-questdb/src/test/java/org/eclipse/jnosql/databases/questdb/integration/SensorReading.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License 2.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at https://www.eclipse.org/legal/epl-2.0
+ *   and the Apache License v2.0 is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ */
+package org.eclipse.jnosql.databases.questdb.integration;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.time.Instant;
+
+@Entity("sensor_mapping")
+public record SensorReading(@Id Instant timestamp, @Column String sensor, @Column double temperature) {
+}
