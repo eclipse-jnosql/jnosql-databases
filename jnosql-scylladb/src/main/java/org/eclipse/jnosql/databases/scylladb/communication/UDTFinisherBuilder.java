dc05d68bab8e201cb451a7962ff05ba35fdc4d82
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-21 07:52:55 +0100

    feat: add UDT builders and User entity for ScyllaDB module

---
+++ jnosql-scylladb/src/main/java/org/eclipse/jnosql/databases/scylladb/communication/UDTFinisherBuilder.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License 2.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at https://www.eclipse.org/legal/epl-2.0
+ *   and the Apache License v2.0 is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.scylladb.communication;
+
+/**
+ * A builder interface for finalizing the creation of a user-defined type (UDT).
+ */
+public interface UDTFinisherBuilder {
+
+    /**
+     * Creates a UDT instance based on the previously specified elements.
+     *
+     * @return a new UDT instance
+     * @throws IllegalStateException if any required element is missing
+     */
+    UDT build() throws IllegalStateException;
+}
---
+++ jnosql-scylladb/src/main/java/org/eclipse/jnosql/databases/scylladb/communication/UDTNameBuilder.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License 2.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at https://www.eclipse.org/legal/epl-2.0
+ *   and the Apache License v2.0 is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.scylladb.communication;
+
+/**
+ * A builder interface for creating a user-defined type (UDT) element with the specified name.
+ */
+public interface UDTNameBuilder {
+
+    /**
+     * Specifies the name of the UDT element.
+     *
+     * @param name the name of the UDT element
+     * @return the {@link UDTElementBuilder} instance to continue building the UDT
+     * @throws NullPointerException if {@code name} is {@code null}
+     */
+    UDTElementBuilder withName(String name) throws NullPointerException;
+}
