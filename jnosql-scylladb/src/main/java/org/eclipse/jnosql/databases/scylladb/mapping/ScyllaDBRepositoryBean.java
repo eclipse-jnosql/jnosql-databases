bf8149fa8a674a34bcaaf1c0abd2002161d4d9bc
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-21 07:52:32 +0100

    feat: add ScyllaDBRepository, ScyllaDBRepositoryBean, and ScyllaDBRepositoryProxyTest for repository-style data access in the ScyllaDB module

---
+++ jnosql-scylladb/src/main/java/org/eclipse/jnosql/databases/scylladb/mapping/ScyllaDBRepositoryBean.java
@@ -0,0 +1,72 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License 2.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at https://www.eclipse.org/legal/epl-2.0
+ *   and the Apache License v2.0 is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.scylladb.mapping;
+
+import jakarta.enterprise.context.spi.CreationalContext;
+import jakarta.enterprise.inject.Default;
+import jakarta.enterprise.util.AnnotationLiteral;
+import org.eclipse.jnosql.mapping.core.spi.AbstractBean;
+import org.eclipse.jnosql.mapping.semistructured.repository.SemistructuredRepositoryProducer;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Set;
+
+
+class ScyllaDBRepositoryBean<T, K> extends AbstractBean<ScyllaDBRepository<T, K>> {
+
+    private final Class<T> type;
+
+    private final Set<Type> types;
+
+    private final Set<Annotation> qualifiers = Collections.singleton(new AnnotationLiteral<Default>() {
+    });
+
+    ScyllaDBRepositoryBean(Class<T> type) {
+        this.type = type;
+        this.types = Collections.singleton(type);
+    }
+
+    @Override
+    public Class<?> getBeanClass() {
+        return type;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public ScyllaDBRepository<T, K> create(CreationalContext<ScyllaDBRepository<T, K>> creationalContext) {
+        var template = getInstance(ScyllaDBTemplate.class);
+        var semiStructuredConverter = getInstance(SemistructuredRepositoryProducer.class);
+        return (ScyllaDBRepository<T, K>) semiStructuredConverter.get(type, template);
+    }
+
+
+    @Override
+    public Set<Type> getTypes() {
+        return types;
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return qualifiers;
+    }
+
+    @Override
+    public String getId() {
+        return type.getName() + "@scylladb";
+    }
+
+}
