137c0b9deb53d16d212c48b795f9e1484a5ffe2a
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-21 07:50:44 +0100

    test: add Constants and Contact classes for ScyllaDB model tests

---
+++ jnosql-scylladb/src/test/java/org/eclipse/jnosql/databases/scylladb/communication/Constants.java
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License 2.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at https://www.eclipse.org/legal/epl-2.0
+ *   and the Apache License v2.0 is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+
+package org.eclipse.jnosql.databases.scylladb.communication;
+
+
+public final class Constants {
+
+    public static final String KEY_SPACE = "newKeySpace";
+    public static final String COLUMN_FAMILY = "newcolumnfamily";
+    private Constants() {}
+}
