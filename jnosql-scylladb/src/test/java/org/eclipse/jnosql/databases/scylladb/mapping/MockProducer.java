512ed7c6215bccae4bc9a20915cc96e2444d82f4
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-21 07:51:33 +0100

    test: add MockProducer, Money model, and mapping properties for ScyllaDB testing

---
+++ jnosql-scylladb/src/test/java/org/eclipse/jnosql/databases/scylladb/mapping/MockProducer.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License 2.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at https://www.eclipse.org/legal/epl-2.0
+ *   and the Apache License v2.0 is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.scylladb.mapping;
+
+
+import jakarta.annotation.Priority;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Produces;
+import jakarta.interceptor.Interceptor;
+import org.eclipse.jnosql.communication.semistructured.CommunicationEntity;
+import org.eclipse.jnosql.communication.semistructured.Element;
+import org.eclipse.jnosql.databases.scylladb.communication.ScyllaDBColumnManager;
+import org.eclipse.jnosql.mapping.Database;
+import org.eclipse.jnosql.mapping.DatabaseType;
+import org.mockito.Mockito;
+
+import java.util.function.Supplier;
+
+import static org.mockito.Mockito.when;
+
+@Alternative
+@Priority(Interceptor.Priority.APPLICATION)
+@Database(DatabaseType.COLUMN)
+public class MockProducer implements Supplier<ScyllaDBColumnManager> {
+
+
+    @Produces
+    @Override
+    public ScyllaDBColumnManager get() {
+        ScyllaDBColumnManager manager = Mockito.mock(ScyllaDBColumnManager.class);
+        var entity = CommunicationEntity.of("Person");
+        entity.add(Element.of("name", "Ada"));
+        when(manager.insert(Mockito.any(CommunicationEntity.class))).thenReturn(entity);
+        return manager;
+    }
+
+}
