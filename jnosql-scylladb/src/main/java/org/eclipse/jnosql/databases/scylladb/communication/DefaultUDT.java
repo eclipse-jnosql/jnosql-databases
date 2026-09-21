14a968613e54328d4f7e54f8c1f1a731741c6f42
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-21 07:51:05 +0100

    test: add unit tests for DefaultScyllaDBTemplate and DefaultUDT classes in ScyllaDB module

---
+++ jnosql-scylladb/src/main/java/org/eclipse/jnosql/databases/scylladb/communication/DefaultUDT.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License 2.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at https://www.eclipse.org/legal/epl-2.0
+ *   and the Apache License v2.0 is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.scylladb.communication;
+
+
+import org.eclipse.jnosql.communication.TypeSupplier;
+import org.eclipse.jnosql.communication.Value;
+import org.eclipse.jnosql.communication.semistructured.Element;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * The default implementation of {@link UDT}
+ */
+class DefaultUDT implements UDT {
+
+    private final String name;
+
+    private final String userType;
+
+    private final List<Element> columns;
+
+    DefaultUDT(String name, String userType, List<Element> columns) {
+        this.name = name;
+        this.userType = userType;
+        this.columns = columns;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public Value value() {
+        return Value.of(columns);
+    }
+
+    @Override
+    public <T> T get(Class<T> clazz) throws NullPointerException, UnsupportedOperationException {
+        Objects.requireNonNull(clazz, "clazz is required");
+        return Value.of(columns).get(clazz);
+    }
+
+    @Override
+    public <T> T get(TypeSupplier<T> typeSupplier) throws NullPointerException, UnsupportedOperationException {
+        Objects.requireNonNull(typeSupplier, "typeSupplier is required");
+        return Value.of(columns).get(typeSupplier);
+
+    }
+
+    @Override
+    public Object get() {
+        return columns;
+    }
+
+    @Override
+    public String userType() {
+        return userType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof UDT udt)) {
+            return false;
+        }
+        return Objects.equals(name, udt.name()) &&
+                Objects.equals(userType, udt.userType()) &&
+                Objects.equals(columns, udt.get());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, userType, columns);
+    }
+
+    @Override
+    public String toString() {
+        return "DefaultUDT{" +
+                "name='" + name + '\'' +
+                ", userType='" + userType + '\'' +
+                ", columns=" + columns +
+                '}';
+    }
+}
